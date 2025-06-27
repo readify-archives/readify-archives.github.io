1e477284ee506ea87b7e736bad11f70b1e1cf2c2
Readify Archives <readify.archives@gmail.com>
2025-06-27 15:35:25 +0530

    V3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-  base: '/readify-archives/',
+  base: '/readify-archives.github.io/',
   plugins: [react()],
 });
